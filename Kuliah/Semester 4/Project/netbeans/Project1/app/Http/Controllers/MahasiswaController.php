bb60dd80fe1e1a0e0d00224fc68b65d1edd717ed
Abdillah Rahman <abdillahrahman2912@gmail.com>
2024-05-03 19:37:26 +0700

    Tambah controller

--- Kuliah/Semester 4/Project/netbeans/Project1/app/Http/Controllers/MahasiswaController.php
+++ Kuliah/Semester 4/Project/netbeans/Project1/app/Http/Controllers/MahasiswaController.php
@@ -1,7 +1,21 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+namespace App\Http\Middleware;
 
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            return route('login');
+        }
+    }
+}
